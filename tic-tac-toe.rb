66d3fc9d2499de2e8f7d195fe811515c57efd1f6
Maurice <mauricezhang2@gmail.com>
2014-06-25 01:47:14 -0400

    Added check for win, move categorization

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,22 +1,22 @@
 class Game
-	def start
 
-		@A1, @B1, @C1, @A2, @B2, @C2, @A3, @B3, @C3 = " ", " ", " ", " ", " ", " ", " ", " ", " "
-
-		board = "   A | B | C \n 1 #{@A1} | #{@B1} | #{@C1}  \n 2 #{@A2} | #{@B2} | #{@C2} \n 3 #{@A3} | #{@B3} | #{@C3} "
-
+    def initialize
+        @places = { 'A1' => " ", 'A2' => " ", 'A3' => " ", 'B1' => " ", 'B2' => " ", 'B3' => " ", 'C1' => " ", 'C2' => " ", 'C3' => " " }
+        @board = "   A | B | C \n 1 #{@places['A1']} | #{@places['B1']} | #{@places['C1']}  \n 2 #{@places['A2']} | #{@places['B2']} | #{@places['C2']} \n 3 #{@places['A3']} | #{@places['B3']} | #{@places['C3']} "
+    end
+    
+    def start
 		puts "Player 1's Name?"
 		firstPlayer = Player.new(gets.chomp, "X")
 		puts "Player 2's Name?"
 		secondPlayer = Player.new(gets.chomp, "O")
 
-		puts board
+		puts @board
 
-		play_turns(firstPlayer, secondPlayer);
-	end
+		play_turns(firstPlayer, secondPlayer)
+    end
 
 	def play_turns(firstPlayer, secondPlayer)
-		
 		currentPlayer = firstPlayer
 
 		until firstPlayer.has_won? || secondPlayer.has_won?
@@ -24,17 +24,17 @@ class Game
 			puts "#{currentPlayer.name}, Enter Your Move:"
 			while move == " "
 				move = gets.chomp
-				if move !=~ /[ABC][123]/
+				unless move =~ /[ABC][123]/ && @places[move] == " "
 					move = " "
-					"Invalid move, try again"
+					puts "Invalid move, try again:"
 				end
 			end
 
-			instance_variable_set("@#{move}", currentPlayer.mark)
-			puts move #testing purposes
-			
-			currentPlayer.moves << move
-			puts board
+			@places[move] = currentPlayer.mark		
+			currentPlayer.add_move(move)
+
+            @board = "   A | B | C \n 1 #{@places['A1']} | #{@places['B1']} | #{@places['C1']}  \n 2 #{@places['A2']} | #{@places['B2']} | #{@places['C2']} \n 3 #{@places['A3']} | #{@places['B3']} | #{@places['C3']} "
+			puts @board
 
 			currentPlayer = (currentPlayer == firstPlayer) ? secondPlayer : firstPlayer
 		end
@@ -45,14 +45,77 @@ class Player
 
 	attr_accessor :name, :mark, :moves
 
-	@moves = []
+    def initialize(name, mark)
+        @name = name
+        @mark = mark
+        @moves = []
+        @columnA, @columnB, @columnC, @row1, @row2, @row3, @diagA, @diagC = [], [], [], [], [], [], [], []
+        @lines = [@columnA, @columnB, @columnC, @row1, @row2, @row3, @diagA, @diagC]
+    end
 
-	def initialize(name, mark)
-		@name = name
-		@mark = mark
-	end
+    def has_won?
+        return false if @moves.size < 3
+        if @lines.any? { |line| line.size >= 3 }
+            puts "#{@name} has won! Type \"play\" to play again!"
+            return true
+        end
+    end
 
-	def has_won?
-		@moves
-	end
+    def add_move(move)
+        @moves << move
+        categorize_move(move)
+    end
+
+    def categorize_move(move)
+
+        case move #Checks for diag
+        when 'A1', 'C3'
+            @diagA << move
+        when 'B2'
+            @diagA << move
+            @diagC << move
+        when 'A3', 'C1'
+            @diagC << move
+        else
+            #error, figure it out later
+        end
+
+        case move[0] # Checks for column
+        when 'A'
+            @columnA << move
+        when 'B'
+            @columnB << move
+        when 'C'
+            @columnC << move
+        else
+            #error, figure it out later
+        end
+
+        case move[1] # Checks for row
+        when '1'
+            @row1 << move
+        when '2'
+            @row2 << move
+        when '3'
+            @row3 << move
+        else
+            #error, figure it out later
+        end
+
+    end
+
 end
+
+def play
+    Game.new.start
+end
+
+puts 'Type "play" to start playing!'
+
+
+
+
+
+
+
+
